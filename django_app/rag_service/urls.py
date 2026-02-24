f63067bd62210814c116e8956040272bbf7af033
khangbkk23 <khang.buitranduycse@hcmut.edu.vn>
2026-02-24 10:55:38 +0700

    update url and app.html for JWT

--- django_app/rag_service/urls.py
+++ django_app/rag_service/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('contact/', views.contact_view, name='contact'),
     path('app/', views.app_view, name='app'),
+    path('api/login/', views.login_endpoint, name='login_api'),
     path('api/chat/', views.chat_endpoint, name='chat_api'),
 ]
